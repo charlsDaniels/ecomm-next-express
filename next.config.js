017caa8d458e4c87f7d1359dc739b0590103c023
Carlos Menendez <cdmenendez@teco.com.ar>
2022-12-19 17:59:00 -0300

    Add environment variables file

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 const nextConfig = {
   env: {
-    firebase_apiKey: "AIzaSyCmaYqDKiTLmsdjfayCfn1ljjSMWhrPmrU",
-    firebase_authDomain: "charltops-ecommerce.firebaseapp.com",
-    firebase_projectId: "charltops-ecommerce",
-    firebase_storageBucket: "charltops-ecommerce.appspot.com",
-    firebase_messagingSenderId: "258733256274",
-    firebase_appId: "1:258733256274:web:13af221ad663898cd3b98b",
+    firebase_apiKey: process.env.FIREBASE_API_KEY,
+    firebase_authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    firebase_projectId: process.env.FIREBASE_PROJECT_ID,
+    firebase_storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    firebase_messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    firebase_appId: process.env.FIREBASE_APP_ID,
   }
 }
 
